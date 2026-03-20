9623c50a5fa79b2c07b47c794f0245fa30392d7a
pin20012 <pin20012@byui.edu>
2026-03-20 12:46:24 -0600

    Update name raw_stocks.sql

--- itm327_dbt_template/models/staging/raw_stocks.sql
+++ itm327_dbt_template/models/staging/raw_stocks.sql
@@ -1,3 +1,3 @@
 -- TODO: Update the source table name to match your prefix (e.g., SMITHJ_STOCKS)
 select *
-from {{ source('snowbearair', 'LASTN_FI_STOCKS') }}
+from {{ source('snowbearair', 'PINNEYZ_STOCKS') }}
