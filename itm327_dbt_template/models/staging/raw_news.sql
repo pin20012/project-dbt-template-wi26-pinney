81d111d220fbc8179b12a7e41f40debda34d0dea
pin20012 <pin20012@byui.edu>
2026-03-20 13:34:43 -0600

    Update raw_news.sql

--- itm327_dbt_template/models/staging/raw_news.sql
+++ itm327_dbt_template/models/staging/raw_news.sql
@@ -1,3 +1,3 @@
 -- TODO: Update the source table name to match your prefix (e.g., SMITHJ_NEWS)
 select *
-from {{ source('snowbearair', 'PINNEYZ_NEWS') }}
+from {{ source('snowbearair', 'NEWS_PINNEY_Z') }}
