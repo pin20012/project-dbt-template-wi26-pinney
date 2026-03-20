e78cf084481c153750bad9d04b21f663c22bd168
pin20012 <pin20012@byui.edu>
2026-03-20 13:35:49 -0600

    Update raw_weather.sql

--- itm327_dbt_template/models/staging/raw_weather.sql
+++ itm327_dbt_template/models/staging/raw_weather.sql
@@ -1,3 +1,3 @@
 -- TODO: Update the source table name to match your prefix (e.g., SMITHJ_WEATHER)
 select *
-from {{ source('snowbearair', 'PINNEYZ_WEATHER') }}
+from {{ source('snowbearair', 'API_WEATHER_TABLE_ZP') }}
